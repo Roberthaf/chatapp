3886804a276454f0e51e9b6970be174120ffd018
Róbert Hafþórsson <roberthafth@gmail.com>
2019-08-01 19:55:08 +0000

    Add login and logic

--- src/Login.js
+++ src/Login.js
@@ -13,7 +13,24 @@ export default class Login extends React.Component{
     render(){
         return(
             <div className="loginContainer">
-
+                <p className="welcomeText">Welcome to ChatApp</p>
+                <form className="login-form" 
+                    onSubmit={(e) => {
+                        e.preventDefault()
+                        this.props.handleLogin(true, this.state.name)
+                    }}>
+                    <label>Please Enter your Name</label>
+                    <input
+                        id="loginInput"
+                        type="text"
+                        placeholder={'Enter your name'}
+                        value={this.state.name}
+                        onChange={e => this.setState({ name: e.target.value })}
+                    >
+                    
+                    </input>
+                    <input type="submit" value={'Login'} className="LoginBotton"/>
+                </form>
             </div>
         )
     }
