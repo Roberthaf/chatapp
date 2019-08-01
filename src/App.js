6d528c6a04ad2d93fe84eb279ecdec73b3f79878
Róbert Hafþórsson <roberthafth@gmail.com>
2019-08-01 19:53:40 +0000

    Change App container to a class and set up imports

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Chat from './Chat';
+import Login from './Login';
 
-function App() {
+export default class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      isLoggedIn : false,
+      name: '',
+    };
+  };
+
+  render() {
+  const {name} = this.state;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <header className="App-header">
+          <h1 className="App-title">Welcome to ChatApp</h1>
+        </header>
+
     </div>
   );
 }
-
-export default App;
+}
