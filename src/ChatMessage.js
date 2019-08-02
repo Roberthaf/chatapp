8cffac10f7e6c72ac05495fd0defac1ce70dc947
Róbert Hafþórsson <roberthafth@gmail.com>
2019-08-02 14:46:04 +0000

    Add styling to ChatMessage
    Changing some styling chocies to make chathistory more readable

--- src/ChatMessage.js
+++ src/ChatMessage.js
@@ -3,7 +3,6 @@ import './ChatMessage.css';
 
 export default ({ mid, name, date, message, editMessage, edited, currentUser,action }) =>
 <li className="Clearfix">  
-  {console.log(action, message)}
   {action === ("userConnected") ?  
     <div className={"Connection-message"}>
       <span>{name} </span> <br />
@@ -22,8 +21,8 @@ export default ({ mid, name, date, message, editMessage, edited, currentUser,act
         <div 
           className={ currentUser===name ? "Message-data" : "Message-data Align-right"}
         >
-          <span>{date}</span> 
-          <span>{name}</span>
+        <span className="Message-data-name">{name}</span>
+        <span className="Message-data-time">{date}</span>   
         </div>
         <div 
           className={ currentUser===name ? "Message My-message" : "Message Others-message Float-right "}
