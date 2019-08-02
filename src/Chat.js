eb3607a62ac9710b126397d13692a71fa08e7d28
Róbert Hafþórsson <roberthafth@gmail.com>
2019-08-02 11:47:19 +0000

    Add a submitMessage function to Chat container

--- src/Chat.js
+++ src/Chat.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './Chat.css';
+import ChatMessage from './ChatMessage';
+import ChatInput from './ChatInput';
 
 const URL = 'ws://10.75.53.114:8080';
 
@@ -16,10 +18,12 @@ export default class Chat extends Component {
     }
 
     this.ws.onmessage = event => {
-      let data = JSON.parse(event.data);
       
+      let data = JSON.parse(event.data);
+      console.log("data",data);
       switch (data.action) {
         case "chatHistory":
+          console.log(data.data);
           this.setState({
             chatHistory: data.data
           });
@@ -35,14 +39,37 @@ export default class Chat extends Component {
     }
   }
 
-  render() {    
+  submitMessage = (message) => {
+    const { name, getDate } = this.props;
+    let date = getDate();
+    const submitmessage = { action: "message", date: date, name: name, message: message, edited: "false"  };
+    this.ws.send(JSON.stringify(submitmessage));
+
+  }
+
+  render() {
+    console.log(this.state.chatHistory);
     return (
       <div>
         <div className="ChatMessage-Container">
-
+          <ul>
+          {this.state.chatHistory.map((message, index) =>
+            <ChatMessage
+            key={index}
+            mid={index}
+            date={message.date}
+            message={message.message}
+            name={message.name}
+            editMessage={this.editMessage}
+            currentUser={this.props.name}
+            />,
+          )}
+          </ul>
         </div>
         <div className="ChatInput-Container">
-
+            <ChatInput 
+              onSubmitMessage ={ message => this.submitMessage(message) }
+            />
         </div>
       </div>
     );
