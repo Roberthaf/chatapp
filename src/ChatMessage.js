24086f94dd9d53fc8c4b23eda6780632798422fa
Róbert Hafþórsson <roberthafth@gmail.com>
2019-08-02 13:26:43 +0000

    Add logic to Chatmessage conect and disconect
    Display a different message styling for both cases.

--- src/ChatMessage.js
+++ src/ChatMessage.js
@@ -1,17 +1,38 @@
 import React from 'react';
 import './ChatMessage.css';
 
-export default ({ mid, name, date, message, editMessage, edited, currentUser }) =>
-  <li className="Clearfix">
-    <div className={ currentUser===name ? "Message-data" : "Message-data Align-right"}>
-      <span>{date}</span> 
-      <span>{name}</span>
-    </div>
-    <div className={ currentUser===name ? "Message My-message" : "Message Others-message Float-right "}
-      onClick={() => editMessage({mid, name, date, message})}
-    >
-      { message }
-      {/* <span>Edit</span> */}
-    </div>
+export default ({ mid, name, date, message, editMessage, edited, currentUser,action }) =>
+<li className="Clearfix">  
+  {console.log(action, message)}
+  {action === ("userConnected") ?  
+    <div className={"Connection-message"}>
+      <span>{name} </span> <br />
+      <span>{date } </span>
+      <span>{message}</span>
+    </div> : null }
+    {action === ("userDisconnected") ?  
+    <div className={"Connection-message"}>
+      <span>{name} </span> <br />
+      <span>{date } </span>
+      <span>{message}</span>
+    </div> : null }
     
-  </li>
+   {action === "message" ? 
+      <div>
+        <div 
+          className={ currentUser===name ? "Message-data" : "Message-data Align-right"}
+        >
+          <span>{date}</span> 
+          <span>{name}</span>
+        </div>
+        <div 
+          className={ currentUser===name ? "Message My-message" : "Message Others-message Float-right "}
+          onClick={() => editMessage({mid, name, date, message})}
+        >
+        {message}
+        </div>
+      </div>
+      : 
+      null 
+    }
+</li>
