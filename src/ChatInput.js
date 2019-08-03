7a32632e8add5d4162030622e9cf57f416289ab3
Róbert Hafþórsson <roberthafth@gmail.com>
2019-08-03 12:26:49 +0000

    Add styling to ChatInput
    Change to textarea from text and style button

--- src/ChatInput.js
+++ src/ChatInput.js
@@ -2,26 +2,40 @@ import React, { Component } from 'react';
 import "./ChatInput.css";
 
 export default class ChatInput extends Component {
-  state = {
-    message: '',
-  };
+  constructor(props){
+    super(props);
+    this.state = {
+      message: '',
+      editmessage: '',
+    };
+  }
+  
 
   render() {
     return (
       <form
-        action="."
+        className="ChatInput-form"
         onSubmit={e => {
           e.preventDefault()
           this.props.onSubmitMessage(this.state.message)
           this.setState({ message: '' })
         }}
       >
-        <input
+        <textarea
+          className="ChatInput-textarea"
           type="text"
           placeholder={'Enter message...'}
           value={this.state.message}
           onChange={e => this.setState({ message: e.target.value })}
-        />
+        >
+
+        </textarea>
+{/*         <input
+          type="text"
+          placeholder={'Enter message...'}
+          value={this.state.message}
+          onChange={e => this.setState({ message: e.target.value })}
+        /> */}
         <input type="submit" value={'Send'} />
       </form>
     );
